0508664463a00b28d5320b5b914adc97a0e62f69
kyourikey <key_kun1@yahoo.co.jp>
2018-11-29 16:44:07 +0900

    切断方式改良

--- Assets/Scripts/CutableObjects/Cube.cs
+++ Assets/Scripts/CutableObjects/Cube.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using Valve.VR.InteractionSystem;
 
 namespace AnythingCut
 {
     public class Cube : CutObject, ICutable
     {
+        enum SplitDirection
+        {
+            Left,
+            Right,
+        }
+        
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Cut();
+            }
+        }
         public override void Cut()
         {
             if (!CanCut())
@@ -20,21 +37,98 @@ namespace AnythingCut
             obj.GetComponent<CutObject>().NowCutLevel = NowCutLevel;
 
             // ReName
-            obj.name = gameObject.name;
+            gameObject.name = "Cube Level : " + NowCutLevel;
+            obj.name = "Cube Level : " + NowCutLevel;
+            
+            // new mesh
+            var mesh = gameObject.GetComponent<MeshFilter>().mesh;
+            obj.GetComponent<MeshFilter>().mesh = CopyMesh(mesh);
 
-            //ReScale
-            gameObject.transform.localScale = gameObject.transform.localScale / 1.2f;
-            obj.transform.localScale = obj.transform.localScale / 1.2f;
-            // RePosition
-            var leftPos = gameObject.transform.TransformPoint(-Vector3.right);
-            var rightPos = gameObject.transform.TransformPoint(Vector3.right);
-            gameObject.transform.position = rightPos;
-            obj.transform.position = leftPos;
+            // mesh adjust
+            AdjustmentMesh(gameObject, SplitDirection.Left);
+            AdjustmentMesh(obj, SplitDirection.Right);
+            //collider adjust
+            AdjustmentCollider(gameObject, SplitDirection.Left);
+            AdjustmentCollider(obj, SplitDirection.Right);
         }
 
         public void TakeCut()
         {
             Cut();
+        }
+
+        private void AdjustmentMesh(GameObject meshGameObject, SplitDirection splitDirection)
+        {
+            var mesh = meshGameObject.GetComponent<MeshFilter>().mesh;
+            if (mesh == null)
+                return;
+
+            var vertices = mesh.vertices;
+            
+            var xMax = vertices.Max(v => v.x);
+            var xMin = vertices.Min(v => v.x);
+            var centerPosition = (xMin + xMax) / 2f;
+
+            for (var i = 0; i < vertices.Length; i++)
+            {
+                switch (splitDirection)
+                {
+                    case SplitDirection.Left:
+                        if (vertices[i].x >= centerPosition)
+                        {
+                            vertices[i].x = centerPosition;
+                        }
+                        break;
+                    case SplitDirection.Right:
+                        if (vertices[i].x <= centerPosition)
+                        {
+                            vertices[i].x = centerPosition;
+                        }
+                        break;
+                }
+            }
+
+            mesh.vertices = vertices;
+            mesh.RecalculateBounds();
+        }
+
+        private void AdjustmentCollider(GameObject colliderGameObject, SplitDirection splitDirection)
+        {
+            var collider = colliderGameObject.GetComponent<BoxCollider>();
+            if (collider == null)
+                return;
+
+            // ReSize
+            var colSize = collider.size;
+            colSize.x /= 2f;
+            collider.size = colSize;
+
+            // ReCenter
+            var mesh = colliderGameObject.GetComponent<MeshFilter>().mesh;
+
+            var vertices = mesh.vertices;
+
+            var xMax = vertices.Max(v => v.x);
+            var xMin = vertices.Min(v => v.x);
+            var centerPosition = (xMin + xMax) / 2f;
+
+            var colCenter = collider.center;
+            colCenter.x = centerPosition;
+            collider.center = colCenter;
+        }
+
+        private static Mesh CopyMesh(Mesh mesh)
+        {
+            var copyMesh = new Mesh
+            {
+                vertices = mesh.vertices,
+                triangles = mesh.triangles,
+                uv = mesh.uv,
+                normals = mesh.normals,
+                colors = mesh.colors,
+                tangents = mesh.tangents
+            };
+            return copyMesh;
         }
     }
 }
